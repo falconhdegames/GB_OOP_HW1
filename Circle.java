28267de53c06ef0b8bd226dc0fb10e84eee474bc
Dmitry <falconhidegames@gmail.com>
2024-03-13 20:43:24 +0300

    Started making Circle

---
+++ Circle.java
@@ -0,0 +1,13 @@
+package HW_1;
+
+public class Circle {
+    public double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+}

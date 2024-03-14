8b5dab0db53b3b596bf1cdf59eee379276634e81
Dmitry <falconhidegames@gmail.com>
2024-03-14 19:05:06 +0300

    Added objects

--- Program.java
+++ Program.java
@@ -2,6 +2,8 @@ package HW_1;
 
 public class Program {
     public static void main(String[] args) {
-        
+        Circle circle = new Circle(10);
+        Rectangle rectangle = new Rectangle(20, 10);
+        Square square = new Square(10);
     }
 }
